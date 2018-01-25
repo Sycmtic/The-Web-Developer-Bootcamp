8f54fe752f4d55b3e8ebdf4176f1193799fe178e
Sycmtic <33992931+Sycmtic@users.noreply.github.com>
2018-01-25 14:12:26 -0800

    Create campgrounds.js

---
+++ Back End/exercise/YelpCamp/v7/routes/campgrounds.js
@@ -0,0 +1,56 @@
+var express     = require("express");
+var router      = express.Router();
+var Campground  = require("../models/campground");
+
+// instead of adding route to app, we add route to router
+// INDEX - show all campgrounds       
+router.get("/", function(req, res) {
+    // Get all campgrounds from DB
+    // use callback function to make sure that only if .find() method finished, the next function execute
+    Campground.find({}, function(err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            // req.user contians the user information
+            res.render("campgrounds/index", {campgrounds: allCampgrounds, currentUser: req.user});
+        }
+    });
+});
+
+// NEW - show form to create new campground
+router.get("/new", function(req, res) {
+    res.render("campgrounds/new");
+});
+
+// CREATE - add new campground to DB
+router.post("/", function(req, res) {
+   var name = req.body.name;
+   var img = req.body.image;
+   var desc = req.body.description;
+   var newCampground = {name: name, image: img, description: desc};
+   // Create a new campground and save to DB
+   // newCampground is the object that save to DB, newlyCreated is the newly added object that returns from DB
+   Campground.create(newCampground, function(err, newlyCreated) {
+       if (err) {
+           console.log(err);
+       } else {
+           // redirect back to campgrounds page
+           res.redirect("/campgrounds");
+       }
+   });
+});
+
+// SHOW - show more info about one campground
+router.get("/:id", function(req, res) {
+    // find the campground with provided Id then get the comment data throgh the reference
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
+        if (err) {
+            console.log(err);
+        } else {
+            // render show the template with that campground
+            res.render("campgrounds/show", {campground: foundCampground});            
+        }
+    });
+});
+
+module.exports = router;
